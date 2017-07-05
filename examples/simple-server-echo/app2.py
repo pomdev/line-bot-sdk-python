0ef54591835e57bc3cc60bcf5e00d9127967a3bc
ek <ek@wealth65.com>
2017-07-05 13:18:49 +0700

    Decorate Code

--- examples/simple-server-echo/app2.py
+++ examples/simple-server-echo/app2.py
@@ -4,12 +4,13 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello World!"
 
-@app.route('/callback', methods=['POST'])
 
+@app.route('/callback', methods=['POST'])
 def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
@@ -19,11 +20,12 @@ def callback():
     sendText(user, 'งง')
     return '', 200
 
+
 def sendText(user, text):
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
     authorization = 'mOO1A40zvrJda8pQtdHRg3t2Ns/ui0axNP1nhiX3z5XWDoCHSP9x99u/XZWG2BfsX9ex4S0P060Hk9RPNXPgc72Sr6VxN+mO6sUhtSf5iSekhT72RI0trKd/HaxJY3dRc90ZUUrNuyG7tkhiqpyvEwdB04t89/1O/w1cDnyilFU='
 
-    headers ={
+    headers = {
         'Content-Type': 'application/json; charset=UTF-8', 'Authorization': authorization
     }
 
@@ -33,4 +35,5 @@ def sendText(user, text):
 
     r = requests.post(LINE_API, headers=headers, data=data)
 
-    if __name__ == '__main__':app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
