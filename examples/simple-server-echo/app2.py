6a0a93e812b4f1541fb05c79b78e6edd71756c93
ek <ek@wealth65.com>
2017-07-05 13:14:23 +0700

    CAP LINE_API

--- examples/simple-server-echo/app2.py
+++ examples/simple-server-echo/app2.py
@@ -20,17 +20,17 @@ def callback():
     return '', 200
 
 def sendText(user, text):
-    line_api = 'https://api.line.me/v2/bot/message/reply'
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
     authorization = 'mOO1A40zvrJda8pQtdHRg3t2Ns/ui0axNP1nhiX3z5XWDoCHSP9x99u/XZWG2BfsX9ex4S0P060Hk9RPNXPgc72Sr6VxN+mO6sUhtSf5iSekhT72RI0trKd/HaxJY3dRc90ZUUrNuyG7tkhiqpyvEwdB04t89/1O/w1cDnyilFU='
 
     headers ={
-        'Content-Type': 'application/json; charset=UTF-8', 'Authorization':authorization
+        'Content-Type': 'application/json; charset=UTF-8', 'Authorization': authorization
     }
 
     data = json.dumps({
         "replyToken": user, "messages": [{"type": "text", "text": text}]
     })
 
-    r = requests.post(line_api, headers=headers, data=data)
+    r = requests.post(LINE_API, headers=headers, data=data)
 
     if __name__ == '__main__':app.run(debug=True)
